f4947adc370bc04884310506e3d709aaee97c255
Luke <luke.bobinski@protonmail.com>
2025-05-07 19:50:05 +0200

    zrobiony klient

--- src/Klient.java
+++ src/Klient.java
@@ -1,4 +1,73 @@
-package PACKAGE_NAME;
-
 public class Klient {
+    private int idKlienta;
+    private String imie;
+    private String nazwisko;
+    private double saldoKonta;
+    private double oprocentowanie;
+
+    public Klient(int idKlienta, String imie, String nazwisko, double saldoKonta, double oprocentowanie){
+        this.idKlienta = idKlienta;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.saldoKonta = saldoKonta;
+        if (oprocentowanie < 0){
+            this.oprocentowanie = 0;
+        } else{
+            this.oprocentowanie = oprocentowanie;
+        }
+
+    }
+    public Klient(int idKlienta, String imie, String nazwisko){
+        this.idKlienta = idKlienta;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.saldoKonta = 0;
+        this.oprocentowanie = 0;
+    }
+    public String toString(){
+        return "Klient ID: " + idKlienta +
+                " imię: " + imie +
+                " nazwisko: " + nazwisko +
+                " saldo: " + saldoKonta +
+                " oprocentowanie: " + oprocentowanie;
+    }
+    public int getIdKlienta() {
+        return idKlienta;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public double getSaldoKonta() {
+        return saldoKonta;
+    }
+
+    public double getOprocentowanie() {
+        return oprocentowanie;
+    }
+
+    public void setIdKlienta(int idKlienta) {
+        this.idKlienta = idKlienta;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setSaldoKonta(double saldoKonta) {
+        this.saldoKonta = saldoKonta;
+    }
+
+    public void setOprocentowanie(double oprocentowanie) {
+        this.oprocentowanie = oprocentowanie;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Klient k1 = new Klient(1, "Lukasz", "Bobinski");
+        System.out.print(k1);
     }
 }
